a7496073ef2639ea02b62e04877716c5dddc95d1
soraiaf <soraiaf2002@gmail.com>
2025-05-27 10:41:26 +0100

    ConsumidorForms

---
+++ Trabalho 2/ConsumidorRabbit/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace ConsumidorRabbit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            btnIniciar = new Button();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(50, 12);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(520, 384);
+            listBox1.TabIndex = 0;
+            // 
+            // btnIniciar
+            // 
+            btnIniciar.Location = new Point(659, 364);
+            btnIniciar.Name = "btnIniciar";
+            btnIniciar.Size = new Size(94, 29);
+            btnIniciar.TabIndex = 1;
+            btnIniciar.Text = "button1";
+            btnIniciar.UseVisualStyleBackColor = true;
+            btnIniciar.Click += new EventHandler(btnIniciar_Click);
+
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnIniciar);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private Button btnIniciar;
+    }
+}
---
+++ Trabalho 2/ConsumidorRabbit/Form1.cs
@@ -0,0 +1,83 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using Microsoft.Data.SqlClient;
+using System.Text;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace ConsumidorRabbit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare(
+                queue: "fila_pecas",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+            );
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (model, ea) =>
+            {
+                try
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    var peca = JsonConvert.DeserializeObject<MensagemPeca>(message);
+
+                    // Apenas mostrar se resultado for diferente de "01"
+                    if (peca.Codigo_Resultado != "01")
+                    {
+                        Invoke((MethodInvoker)delegate
+                        {
+                            listBox1.Items.Add($"Falha: {peca.Codigo_Peca} | Resultado: {peca.Codigo_Resultado}");
+                        });
+                    }
+
+                    // Inserir na base de dados "Producao2", tabela Produto
+                    using (SqlConnection conn = new SqlConnection("Server=localhost;Database=Producao2;Trusted_Connection=True;TrustServerCertificate=True;"))
+                    {
+                        conn.Open();
+                        SqlCommand cmd = new SqlCommand(
+                            "INSERT INTO Produto (Codigo_Peca, Data_Producao, Hora_Producao, Tempo_Producao, Codigo_Resultado) " +
+                            "VALUES (@Codigo, @Data, @Hora, @Tempo, @Resultado)", conn);
+
+                        cmd.Parameters.AddWithValue("@Codigo", peca.Codigo_Peca);
+                        cmd.Parameters.AddWithValue("@Data", DateTime.Now.Date);
+                        cmd.Parameters.AddWithValue("@Hora", DateTime.Now.TimeOfDay);
+                        cmd.Parameters.AddWithValue("@Tempo", peca.Tempo_Producao);
+                        cmd.Parameters.AddWithValue("@Resultado", peca.Codigo_Resultado);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro no consumidor: " + ex.Message);
+                }
+            };
+
+            channel.BasicConsume(
+                queue: "fila_pecas",
+                autoAck: true,
+                consumer: consumer
+            );
+
+            MessageBox.Show("Consumo iniciado com sucesso!");
+        }
+    }
+}
