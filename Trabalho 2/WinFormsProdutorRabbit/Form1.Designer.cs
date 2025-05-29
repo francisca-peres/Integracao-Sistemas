4ff0097d8438647a586ff5751787058de18fb1e3
francisca-peres <franciscaperes2@gmail.com>
2025-05-29 14:15:31 +0100

    atualizacao

--- Trabalho 2/WinFormsProdutorRabbit/Form1.Designer.cs
+++ Trabalho 2/WinFormsProdutorRabbit/Form1.Designer.cs
@@ -32,18 +32,21 @@
             txtCodigoPeca = new TextBox();
             txtTempo = new TextBox();
             txtResultado = new TextBox();
-            txtTipo = new TextBox();
             label2 = new Label();
             label3 = new Label();
-            label4 = new Label();
             btnEnviar = new Button();
+            txtData = new TextBox();
+            txtHora = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
+            btnPreencher = new Button();
             label1 = new Label();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(103, 66);
+            label1.Location = new Point(90, 170);
             label1.Name = "label1";
             label1.Size = new Size(113, 20);
             label1.TabIndex = 4;
@@ -51,36 +54,32 @@
             // 
             // txtCodigoPeca
             // 
-            txtCodigoPeca.Location = new Point(223, 62);
+            txtCodigoPeca.Location = new Point(223, 163);
             txtCodigoPeca.Name = "txtCodigoPeca";
+            txtCodigoPeca.ReadOnly = true;
             txtCodigoPeca.Size = new Size(125, 27);
             txtCodigoPeca.TabIndex = 0;
             // 
             // txtTempo
             // 
-            txtTempo.Location = new Point(223, 118);
+            txtTempo.Location = new Point(223, 211);
             txtTempo.Name = "txtTempo";
+            txtTempo.ReadOnly = true;
             txtTempo.Size = new Size(125, 27);
             txtTempo.TabIndex = 1;
             // 
             // txtResultado
             // 
-            txtResultado.Location = new Point(223, 177);
+            txtResultado.Location = new Point(223, 256);
             txtResultado.Name = "txtResultado";
+            txtResultado.ReadOnly = true;
             txtResultado.Size = new Size(125, 27);
             txtResultado.TabIndex = 2;
-            // 
-            // txtTipo
-            // 
-            txtTipo.Location = new Point(223, 242);
-            txtTipo.Name = "txtTipo";
-            txtTipo.Size = new Size(125, 27);
-            txtTipo.TabIndex = 3;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(60, 125);
+            label2.Location = new Point(60, 218);
             label2.Name = "label2";
             label2.Size = new Size(143, 20);
             label2.TabIndex = 5;
@@ -89,20 +88,11 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(75, 184);
+            label3.Location = new Point(75, 259);
             label3.Name = "label3";
             label3.Size = new Size(128, 20);
             label3.TabIndex = 6;
             label3.Text = "Código Resultado";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(125, 242);
-            label4.Name = "label4";
-            label4.Size = new Size(39, 20);
-            label4.TabIndex = 7;
-            label4.Text = "Tipo";
             // 
             // btnEnviar
             // 
@@ -112,20 +102,65 @@
             btnEnviar.TabIndex = 8;
             btnEnviar.Text = "Enviar";
             btnEnviar.UseVisualStyleBackColor = true;
-            btnEnviar.Click += new EventHandler(btnEnviar_Click);
-
+            btnEnviar.Click += btnEnviar_Click;
+            // 
+            // txtData
+            // 
+            txtData.Location = new Point(223, 70);
+            txtData.Name = "txtData";
+            txtData.ReadOnly = true;
+            txtData.Size = new Size(125, 27);
+            txtData.TabIndex = 9;
+            // 
+            // txtHora
+            // 
+            txtHora.Location = new Point(223, 119);
+            txtHora.Name = "txtHora";
+            txtHora.ReadOnly = true;
+            txtHora.Size = new Size(125, 27);
+            txtHora.TabIndex = 10;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(162, 70);
+            label4.Name = "label4";
+            label4.Size = new Size(41, 20);
+            label4.TabIndex = 11;
+            label4.Text = "Data";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(153, 126);
+            label5.Name = "label5";
+            label5.Size = new Size(42, 20);
+            label5.TabIndex = 12;
+            label5.Text = "Hora";
+            // 
+            // btnPreencher
+            // 
+            btnPreencher.Location = new Point(517, 256);
+            btnPreencher.Name = "btnPreencher";
+            btnPreencher.Size = new Size(94, 29);
+            btnPreencher.TabIndex = 13;
+            btnPreencher.Text = "Preencher";
+            btnPreencher.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(btnEnviar);
+            Controls.Add(btnPreencher);
+            Controls.Add(label5);
             Controls.Add(label4);
+            Controls.Add(txtHora);
+            Controls.Add(txtData);
+            Controls.Add(btnEnviar);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
-            Controls.Add(txtTipo);
             Controls.Add(txtResultado);
             Controls.Add(txtTempo);
             Controls.Add(txtCodigoPeca);
@@ -140,10 +175,13 @@
         private TextBox txtCodigoPeca;
         private TextBox txtTempo;
         private TextBox txtResultado;
-        private TextBox txtTipo;
         private Label label2;
         private Label label3;
-        private Label label4;
         private Button btnEnviar;
+        private TextBox txtData;
+        private TextBox txtHora;
+        private Label label4;
+        private Label label5;
+        private Button btnPreencher;
     }
 }
--- Trabalho 2/WinFormsProdutorRabbit/Form1.cs
+++ Trabalho 2/WinFormsProdutorRabbit/Form1.cs
@@ -8,25 +8,123 @@ namespace WinFormsProdutorRabbit
 {
     public partial class Form1 : Form
     {
+        private System.Windows.Forms.Timer autoTimer = new System.Windows.Forms.Timer();
+        private Random random = new Random();
+        private int pecasGeradas = 0;
+        private const int limitePecas = 3;
+        private bool prontoParaEnviar = false;
+
         public Form1()
         {
             InitializeComponent();
+
+            // Configura o timer
+            autoTimer.Interval = 5000; // 5 segundos
+            autoTimer.Tick += AutoTimer_Tick;
+
+            // Liga o botão Preencher ao evento
+            btnPreencher.Click += btnPreencher_Click;
+        }
+
+        private void btnPreencher_Click(object sender, EventArgs e)
+        {
+            pecasGeradas = 0;
+
+            // Gerar a primeira peça
+            PreencherCamposAutomaticamente();
+
+            // Indicar que a próxima iteração será de envio, não de geração
+            prontoParaEnviar = true;
+
+            // Começar o ciclo
+            autoTimer.Start();
+        }
+
+        private void AutoTimer_Tick(object sender, EventArgs e)
+        {
+            if (pecasGeradas >= limitePecas)
+            {
+                autoTimer.Stop();
+                MessageBox.Show("Geração automática concluída.");
+                return;
+            }
+
+            if (prontoParaEnviar)
+            {
+                btnEnviar_Click(null, null); // Envia os dados da peça
+                pecasGeradas++;
+
+                // Só limpa os campos se ainda houver mais peças a gerar
+                if (pecasGeradas < limitePecas)
+                {
+                    LimparCampos();
+                }
+            }
+            else
+            {
+                PreencherCamposAutomaticamente(); // Preenche nova peça visível
+            }
+
+            prontoParaEnviar = !prontoParaEnviar; // Alterna entre gerar e enviar
+        }
+
+        private void PreencherCamposAutomaticamente()
+        {
+            DateTime agora = DateTime.Now;
+
+            txtData.Text = agora.ToString("yyyy-MM-dd");
+            txtHora.Text = agora.ToString("HH:mm:ss");
+
+            string[] tipos = { "aa", "ab", "ba", "bb" };
+            string tipo = tipos[random.Next(tipos.Length)];
+            string identificador = random.Next(100000, 999999).ToString();
+
+            txtCodigoPeca.Text = tipo + identificador;
+            txtTempo.Text = random.Next(10, 51).ToString();
+            txtResultado.Text = random.Next(1, 7).ToString("D2");
+        }
+
+        private void LimparCampos()
+        {
+            txtData.Clear();
+            txtHora.Clear();
+            txtCodigoPeca.Clear();
+            txtTempo.Clear();
+            txtResultado.Clear();
         }
 
         private void btnEnviar_Click(object sender, EventArgs e)
         {
             try
             {
+                if (!DateTime.TryParse(txtData.Text, out DateTime data))
+                {
+                    MessageBox.Show("Data inválida.");
+                    return;
+                }
+
+                if (!TimeSpan.TryParse(txtHora.Text, out TimeSpan hora))
+                {
+                    MessageBox.Show("Hora inválida.");
+                    return;
+                }
+
+                if (!int.TryParse(txtTempo.Text, out int tempoProducao))
+                {
+                    MessageBox.Show("Tempo de produção inválido.");
+                    return;
+                }
+
                 var dados = new
                 {
+                    Data_Producao = data.ToString("yyyy-MM-dd"),
+                    Hora_Producao = hora.ToString(),
                     Codigo_Peca = txtCodigoPeca.Text,
-                    Tempo_Producao = txtTempo.Text,
-                    Codigo_Resultado = txtResultado.Text,
-                    Tipo = txtTipo.Text
+                    Tempo_Producao = tempoProducao,
+                    Codigo_Resultado = txtResultado.Text
                 };
 
                 var json = JsonConvert.SerializeObject(dados);
-                MessageBox.Show("JSON gerado: " + json); // DEBUG
 
                 var factory = new ConnectionFactory() { HostName = "localhost" };
 
@@ -51,7 +149,7 @@ namespace WinFormsProdutorRabbit
                     );
                 }
 
-                MessageBox.Show("Mensagem enviada com sucesso!");
+                Console.WriteLine($"Mensagem enviada: {json}");
             }
             catch (Exception ex)
             {
